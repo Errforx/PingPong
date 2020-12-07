7d07f39a569f43dec1d50ea2485946c19b624ddc
Errforx@Wh4t3v3r <68257861+Errforx@users.noreply.github.com>
2020-12-07 21:31:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const h = document.getElementById('h');
 const ctx = canvas.getContext('2d');
 function updateSize() {
     canvas.style.width = window.innerWidth - 70 + "px";
-    canvas.style.height = window.innerHeight - 145 + "px";
+    canvas.style.height = window.innerHeight - 200 + "px";
 }
 
 function help() {
